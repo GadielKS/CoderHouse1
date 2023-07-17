2c03f4357d28bfce935154482ade1d34a5dc14fa
GadielKS <72422920+GadielKS@users.noreply.github.com>
2023-07-17 18:52:28 +0000

    Version 0.1

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from sqlalchemy import create_engine
 url = "https://api.etherscan.io/api"
 
 parametros_ultimo_bloque = {
-    "module": "proxy",
-    "action": "eth_blockNumber",
-    "apikey": "3XKGEHIXCRBD58B8K1GY9YRT7KDHG95Y3F"  # Reemplaza con tu clave API
+  "module": "proxy",
+  "action": "eth_blockNumber",
+  "apikey": "3XKGEHIXCRBD58B8K1GY9YRT7KDHG95Y3F"  # Reemplaza con tu clave API
 }
 
 # Realiza la solicitud para obtener el número del último bloque
@@ -16,43 +16,46 @@ respuesta = requests.get(url, params=parametros_ultimo_bloque)
 
 # Verifica que la solicitud haya sido exitosa
 if respuesta.status_code == 200:
-    datos = respuesta.json()
+  datos = respuesta.json()
 
-    # Obtiene el número del último bloque
-    ultimo_bloque = datos['result']
+  # Obtiene el número del último bloque
+  ultimo_bloque = datos['result']
 
-    parametros_bloque_especifico = {
-        "module": "proxy",
-        "action": "eth_getBlockByNumber",
-        "tag": ultimo_bloque,
-        "boolean": "true",
-        "apikey": "3XKGEHIXCRBD58B8K1GY9YRT7KDHG95Y3F"  # Reemplaza con tu clave API
-    }
+  parametros_bloque_especifico = {
+    "module": "proxy",
+    "action": "eth_getBlockByNumber",
+    "tag": ultimo_bloque,
+    "boolean": "true",
+    "apikey":
+    "3XKGEHIXCRBD58B8K1GY9YRT7KDHG95Y3F"  # Reemplaza con tu clave API
+  }
 
-    # Realiza la solicitud para obtener la información del último bloque
-    respuesta_bloque = requests.get(url, params=parametros_bloque_especifico)
+  # Realiza la solicitud para obtener la información del último bloque
+  respuesta_bloque = requests.get(url, params=parametros_bloque_especifico)
 
-    if respuesta_bloque.status_code == 200:
-        datos_bloque = respuesta_bloque.json()
+  if respuesta_bloque.status_code == 200:
+    datos_bloque = respuesta_bloque.json()
 
-        # Procesa los datos JSON a un diccionario de Python
-        diccionario = dict(datos_bloque)
+    # Procesa los datos JSON a un diccionario de Python
+    diccionario = dict(datos_bloque)
 
-        # Intenta convertir los datos en un DataFrame
-        try:
-            df = pd.DataFrame([diccionario['result']])
-            # Transforma el timestamp de segundos desde epoch a formato timestamp
-        except ValueError as e:
-            print(f"Error al convertir los datos en un DataFrame: {e}")
+    # Intenta convertir los datos en un DataFrame
+    try:
+      df = pd.DataFrame([diccionario['result']])
+      # Transforma el timestamp de segundos desde epoch a formato timestamp
+    except ValueError as e:
+      print(f"Error al convertir los datos en un DataFrame: {e}")
 
-    else:
-        print(f"Error en la petición del bloque: {respuesta_bloque.status_code}")
+  else:
+    print(f"Error en la petición del bloque: {respuesta_bloque.status_code}")
 else:
-    print(f"Error en la petición del número de bloque: {respuesta.status_code}")
+  print(f"Error en la petición del número de bloque: {respuesta.status_code}")
 
 # Definir las credenciales de la base de datos Redshift
 
-engine = create_engine('redshift+psycopg2://scharfgadiel_coderhouse:JTD9823vUq@data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com:5439/data-engineer-database')
+engine = create_engine(
+  'redshift+psycopg2://scharfgadiel_coderhouse:JTD9823vUq@data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com:5439/data-engineer-database'
+)
 
 # Define la consulta para crear la tabla
 create_table_query = """
@@ -88,8 +91,7 @@ connection.execute(create_table_query)
 
 # Ejecuta la consulta para crear la tabla
 with engine.connect() as connection:
-    connection.execute(create_table_query)
+  connection.execute(create_table_query)
 
 # Usa pandas para escribir los datos del DataFrame a la base de datos
 df.to_sql('entregable_1', con=engine, if_exists='append', index=False)
-
